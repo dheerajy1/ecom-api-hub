4b98462320678be23a29021c4ed2c6367c78b6d5
dheerajy <yssdhiraj@gmail.com>
2025-12-20 19:33:41 +0530

    fix: eliminate module instantiation error by removing barrel imports
    - Remove `src/routes/index.ts` barrel file
    - Update `src/index.ts` to use direct imports for routers
    - Resolve "Requested module is not instantiated yet" crash on Vercel/Bun

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@ import { connectDB } from "./lib/mongo.js";
 import { apiRateLimit, openApiAccess, serverGate } from "./middleware/index.js";
 import { generateOpenAPISpec } from "./orpc/openapi.js";
 import { orpcRouter } from "./orpc/router.js";
-import { aboutRouter, healthzRouter, homeRouter, loginRouter } from "./routes/index.js";
+import aboutRouter from "./routes/about.js";
+import healthzRouter from "./routes/healthz.js";
+import homeRouter from "./routes/home.js";
+import loginRouter from "./routes/login.js";
 
 const app = express();
 
--- src/routes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as aboutRouter } from "./about.js";
-export { default as healthzRouter } from "./healthz.js";
-export { default as homeRouter } from "./home.js";
-export { default as loginRouter } from "./login.js";
